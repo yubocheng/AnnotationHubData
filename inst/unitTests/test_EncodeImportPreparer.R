c0ac17deed8e683a6b610281e03ce9eee69ddc8c
Paul Shannon <pshannon@fhcrc.org>
2013-03-01 23:36:28 +0000

    removed two obsolete tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@73865 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_EncodeImportPreparer.R
+++ inst/unitTests/test_EncodeImportPreparer.R
@@ -29,111 +29,7 @@ paulsTests <- function()
     test_.mapFormatToRecipe()
     test_encodeMetadataToAnnotationHubMetadata()
     
-    #test_assemblParams()
-    #test_createEncodeResource.1()
-  
 } # runTests
-#-------------------------------------------------------------------------------
-test_assemblParams <- function()
-{
-    print('--- test_assembleParams')
-
-    importer <- EncodeImportPreparer()
-    downloadDir <- system.file("unitTests/cases/encodeDCCMetadata",
-                               package="AnnotationHubData")
-    result <-
-        AnnotationHubData:::.learnAllEncodeMetadataCategories(downloadDir,
-                                                              verbose=FALSE)
-    metadata.files <- dir(downloadDir)
-    stopifnot(length(metadata.files) == 2)
-    full.paths <- file.path(downloadDir, metadata.files)
-    
-    importer <- EncodeImportPreparer()
-    tbl <- parseMetadataFiles(importer, full.paths, result$all.keys)
-
-
-    tbl.md <- metadataTable(importer)
-    
-    annotationHubRoot <- "/foo/bar"
-    webSiteRoot <- "http://hgdownload.cse.ucsc.edu"
-        # check this against info, but don't always believe it!
-    genomeVersion <- "hg19"
-    dataFileName <- "wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna.broadPeak.gz"
-
-
-    experiment.md <- as.list(tbl.md[dataFileName,])
-    projectName <- experiment.md$dataDir
-    projectPath <- file.path("goldenpath", genomeVersion, "encodeDCC", projectName)
-    localStorageDirectory <- file.path(annotationHubRoot, projectPath)
-    webSiteSourceDirectory <- file.path(webSiteRoot, projectPath)
-    
-    checkEquals(localStorageDirectory, "/foo/bar/goldenpath/hg19/encodeDCC/wgEncodeSunyAlbanyGeneSt")
-    checkEquals(projectPath, "goldenpath/hg19/encodeDCC/wgEncodeSunyAlbanyGeneSt")
-
-    params <- assembleParams(importer, experiment.md, webSiteSourceDirectory,
-                             annotationHubRoot, projectPath, genomeVersion,
-                             dataFileName)
-    
-    with(params, {
-        checkEquals(Species, "Homo sapiens");
-        checkEquals(Genome, "hg19");
-        checkEquals(Recipe, "extendedBedToGRanges");
-        checkTrue(is.list(RecipeArgs))
-        checkEquals(RDataClass, "GRanges");
-        checkEquals(RDataVersion, "0.0.1");
-        checkEquals(Maintainer, "Paul Shannon <pshannon@fhcrc.org>");
-        checkEquals(DataProvider, "hgdownload.cse.ucsc.edu");
-        checkEquals(Coordinate_1_based, FALSE);
-        checkEquals(RDataDateAdded, as.character(Sys.Date()))
-        checkEquals(AnnotationHubRoot, annotationHubRoot)
-        checkEquals(SourceFile,
-              "goldenpath/hg19/encodeDCC/wgEncodeSunyAlbanyGeneSt/wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna.broadPeak.gz");
-        checkEquals(Title, "wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna");
-        checkEquals(Description,
-              "RbpAssocRna broadPeak H1-hESC GSM787620 wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna RipGeneSt wgEncodeEH001235 SunyAlbany");
-        checkEquals(SourceUrl,
-              "http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/wgEncodeSunyAlbanyGeneSt/wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna.broadPeak.gz");
-        checkEquals(SourceVersion, "Nov_69522,Mil_AB3790 antibodies");
-        })
-    
-
-} # test_assembleParams
-#-------------------------------------------------------------------------------
-notest_createEncodeResource.1 <- function()
-{
-    print('--- test_createEncodeResource.1')
-    importer <- EncodeImportPreparer()
-    tbl.md <- metadataTable(importer)
-    
-    dataFileName <- "wgEncodeSunyAlbanyGeneStH1hescT7tagRbpAssocRna.broadPeak.gz"
-    checkTrue(dataFileName %in% rownames(tbl.md))
-    
-    directory <- tbl.md[dataFileName, "dataDir"]
-    experiment.metadata <- as.list(tbl.md[dataFileName,])
-
-    annotationHubRoot.tmp <- tempdir()
-
-    webSiteRoot <- "http://hgdownload.cse.ucsc.edu"
-
-    checkEquals(experiment.metadata$origAssembly, "hg18")
-    genomeVersion <- "hg19"
-
-    RDataFilename <- createResource(importer, annotationHubRoot.tmp,
-                                    webSiteRoot, genomeVersion,
-                                    dataFileName, experiment.metadata,
-                                    insertIntoDatabase=FALSE,
-                                    verbose=FALSE)
-
-    load(RDataFilename)
-    checkTrue(exists('gr'))
-    checkEquals(length(gr), 147)
-    md <- mcols(gr)
-    checkEquals(dim(md), c(147, 5))
-    checkEquals(colnames(md), c("name", "score", "signalValue", "pValue", "qValue"))
-
-    TRUE
-
-} # test_createEncodeResource.1 
 #-------------------------------------------------------------------------------
 # ensure that, in simple web page crawling, we can extract
 #   "wgEncodeAffyRnaChip/"
@@ -346,7 +242,9 @@ test_encodeMetadataToAnnotationHubMetadata <- function()
     print("--- test_encodeMetadataToAnnotationHubMetadata")
 
     downloadDir <- system.file("unitTests/cases/encodeDCCMetadata", package="AnnotationHubData")
-    load(file.path(downloadDir, "tbl.mdTest.RData"))
+    full.path <- file.path(downloadDir, "tbl.mdTest.RData")
+
+    load(
     checkEquals(dim(tbl.mdTest), c(2,51))
     importPrep <- EncodeImportPreparer(tbl.mdTest)
 
