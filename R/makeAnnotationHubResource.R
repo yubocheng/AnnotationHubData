3dcfdc3eb93d480765331a935ab5abfe8724e118
Marc Carlson <mcarlson@fhcrc.org>
2014-01-22 23:40:06 +0000

    Adds psuedocode for plans to make adding new recipes easier.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@85730 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/makeAnnotationHubResource.R
@@ -0,0 +1,143 @@
+## Goals:
+
+## makeAnnotationHubResource is to help people who are external to
+## AnnotationHubData to be able to define recipes that can then be
+## used by the package. The function takes three arguments: the 1st is
+## an importPreparer object (basically a name that they will have to
+## define as a class).  The 2nd is a function that describes how to
+## make an AnnotationHubMetadata object from external files (on web).
+## And the 3rd is a function that describes the recipe, or how you
+## take that metadata and convert it into an R object that should be
+## saved in the AnnotationHub.
+
+
+
+## makeAnnotationHubResource will move the multiple resources ->
+## multiple AHMs part down a level so that the user only has to define
+## how to do "one" resource (by each function), and in a vectorized way???
+
+## Unless I use a ..., these two function arguments need to have the
+## property that they can't require any arguments.
+
+## And they must each return a specific thing (AHM in the case of
+## argument #2 - which will be checked for in that case).
+
+## In this design, we still keep the preparer code separated from the
+## code for the recipe (so that the two can be re-used if
+## appropriate).
+
+##############################################################################
+## Return value (or what it does after making an/many AHMs):
+## makeAnnotationHubResource will have to define a newResources method
+## for each AHM type.  It can either return that object OR
+## alternatively, it could use a template to spit their code into a
+## separarate module/package.  OR, I guess the meat of this
+## information could be stored in a DB and that DB could be processed
+## to extract data again later.  For now I am planning on just
+## defining a method in the helper function.
+
+## So it seems that we CAN define a method inside our helper and still
+## have it be usable elsewhere...
+## Some tests
+## library(AnnotationHubData)
+## fred <- setClass("fred", contains="ImportPreparer")
+## testMethDef <-function(name){
+##     setMethod(newResources, name, 
+##               function(importPreparer, currentMetadata = list(), ...){
+##               })
+## }
+
+## testMethDef("fred")
+## getMethods(f="newResources")
+
+## BUT to take advantage of that I will have to basically define a
+## single newResourcesMethod that can work for all of them.
+
+
+###############################################################################
+## AND: for a test case, lets re-implement the ensembl GTF preparer
+## using this new interface.
+
+
+
+
+makeAnnotationHubResource <-
+    function(objName,
+             makeAnnotationHubMetadataFunction)#,
+             ## recipeFunction)
+{
+    ## make the object
+    obj <- setClass(objName, contains="ImportPreparer")
+
+    
+    ## Create a newResources Method for the object type passed in.
+    ## The job of this method is to only get resources that are "new"
+    ## It takes an arg of "old" AHMs that can be used for filtering.    
+    ## So it will call the makeAnnotationHubMetadataFunction, and then
+    ## toss out any currentMetadata() AHMs that are already present.
+    setMethod(newResources, objName, 
+       function(importPreparer, currentMetadata = list(), ...){
+           
+           ## ## Get the recipe name and the package its defined in
+           ## recipeName <- as.character(substitute(recipeFunction))
+           ## recipePackage <- packageName(environment(recipeFunction))
+           ## if (recipePackage == ".GlobalEnv")
+           ##     stop("'recipeFunction' must be defined in a package")
+           
+           ## The 1st function can take no args and must return AHMs
+           ahms <- makeAnnotationHubMetadataFunction()
+           
+           ## ## set up the recipes here (*IF* they are not
+           ## ## required to be set up BEFOREHAND)
+           ## for (ahm in ahms)
+           ##     recipe(ahm) <- c(recipeName, package=recipePackage)
+
+           ## And only return ones we don't have.
+           setdiff(ahms, currentMetadata)
+       })
+}
+
+
+
+
+################################################################################
+## LONG FORM of the function call (requires less knowledge by the user)
+
+## Of course another way to think about this would be to just allow
+## users to fill in a 'form' that would basically define the
+## 'metadata' for them.  then the 2nd argument could be replaced with
+## generic code and the user would just fill in a bunch of arguments
+## along with a function for the recipe of what to do with the data.
+## So FOR THE FUTURE: this might be a good way to wrap the helper that
+## I am sketching out right now (with code that will allow a user to
+## basically give vectorized arguments to this helper).
+
+
+## genericAnnotationHubMetadataMapper <- function(meta1, meta2){
+##     ## generic Map call to replace most AHM construction operations.
+## }
+
+## makeAnnotationHubResourceFromParams <-
+##     function(objName,
+##              recipeFunction,
+##              meta1,
+##              meta2,
+##              meta3)
+## {
+##     ## get metadata as AHMs
+##     AHMs <- genericAnnotationHubMetadataFunction(meta1,
+##                                                  meta2,
+##                                                  meta3)    
+##     ## convert metadata arguments with a generic utility function
+##     makeAnnotationHubResource(objName,
+##                               AHMs,
+##                               recipeFunction)
+## }
+
+
+## Seeing this eventual abstraction, I see that I really want my base
+## function to take a series of AHMs (IOW not a function).  And that I
+## really want to make that part generic for the 1st pass (not as a
+## thin wrapper later on).
+
+
---
+++ R/makeEnsemblGtfToGRanges.R
@@ -0,0 +1,128 @@
+## This is an example of how this new helper method can make things
+## simpler and also provides a test case for how we can parse ensembl
+## GTF files into GRanges objects.
+
+
+## STEP 1: come up with a name for the new ImportPreparer class
+## "EnsemblGtfImportPreparer"
+
+
+
+## STEP 2: make function to process metadata into AHMs
+
+## This helper is verbatim from Martin and returns ALL the potential sourceUrls.
+## .ensemblGtfSourceUrls <-
+##     function(baseUrl)
+## {
+##     want <- .ensemblDirUrl(baseUrl, "gtf/")
+##     ## files in release
+##     unlist(lapply(want, function(url) {
+##         listing <- getURL(url=url, followlocation=TRUE, customrequest="LIST -R")
+##         listing<- strsplit(listing, "\n")[[1]]
+##         subdir <- sub(".* ", "", listing[grep("^drwx", listing)])
+##         gtfGz <- sub(".* ", "", listing[grep(".*gtf.gz$", listing)])
+##         sprintf("%s%s/%s", url, subdir, gtfGz)
+##     }), use.names=FALSE)
+## }
+
+## This function will return the AHMs and takes no args...
+makeEnsemblAHMsFromGTFs <- function(){
+    baseUrl <- .ensemblBaseUrl
+    sourceUrl <- .ensemblGtfSourceUrls(.ensemblBaseUrl)
+    
+    sourceFile <- .ensemblSourcePathFromUrl(baseUrl, sourceUrl)
+    meta <- .ensemblMetadataFromUrl(sourceUrl)
+    rdata <- sub(".gz$", ".RData", sourceFile)
+    description <- paste("Gene Annotation for", meta$species)
+
+    Map(AnnotationHubMetadata,
+        AnnotationHubRoot=meta$annotationHubRoot,
+        Description=description, Genome=meta$genome,
+        SourceFile=sourceFile, SourceUrl=sourceUrl,
+        SourceVersion=meta$sourceVersion, Species=meta$species,
+        TaxonomyId=meta$taxonomyId, Title=meta$title,
+        MoreArgs=list(
+          Coordinate_1_based = TRUE,
+          DataProvider = "ftp.ensembl.org",
+          Maintainer = "Martin Morgan <mtmorgan@fhcrc.org>",
+          RDataClass = "GRanges",
+          RDataDateAdded = Sys.time(),
+          RDataVersion = "0.0.1",
+          Recipe = c("ensemblGtfToGRangesRecipe", package="AnnotationHubData"),
+          Tags = c("GTF", "ensembl", "Gene", "Transcript", "Annotation")))
+}
+
+## TODO: modify recipe above to not do the Recipe? OR make
+## newResources so that it stops worrying about recipes?  CHOOSE...
+
+
+## STEP 3:  Make a recipe that takes an AnnotationHubRecipe object.
+## Yes I am keeping things so that users have to learn about
+## AnnotationHubRecipe objects BECAUSE I plan to remove from users the
+## need to know about the more complicated AMH objects.
+ensemblGTFToGRangesRecipe <- function(recipe){
+    require(rtracklayer)
+    gz.inputFile <- inputFiles(recipe)[1]
+    con <- gzfile(gz.inputFile)
+    on.exit(close(con))
+    gr <- import(con, "gtf", asRangedData=FALSE)
+    save(gr, file=outputFile(recipe))
+    outputFile(recipe)
+}
+
+
+
+## STEP 4:  Call the helper to set up the newResources() method
+## makeAnnotationHubResource("EnsemblGtfImportPreparer",
+##                           makeEnsemblAHMsFromGTFs,
+##                           ensemblGTFToGRangesRecipe)
+
+
+
+## TODO: choose whether to have separate arg for recipe or to have it be referenced by the other function (leaning towards the 2nd option right now)...
+
+
+
+
+##############################################################################
+## NEXT: make a long-form call and skip the AHM function helper
+## function (step 2) entirely.
+## RESULT: a THREE step process that eliminates the need for users to
+## know about AHMs
+
+## makeAnnotationHubResourceFromParams("EnsemblGtfImportPreparer",
+##                                     ensemblGTFToGRangesRecipe,
+##                                     meta1,
+##                                     meta2,
+##                                     meta3)
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+## 'NEXT' NEXT: Factor out the need for the user to know about
+## AnnotationHubRecipe objects by making a wrapper for creating
+## recipes? OR just explain what an AnnotationHubRecipe is...
+
+## Right now I am leaning towards factoring it out.  Because I would
+## rather just describe 4 arguments than explain this new class to
+## users...  And this class contains no info that is not already
+## derived from an AHM (along with an AHM).
+
+## BUT LURKING in the code the recipes are all expecting an AHMR, so I
+## will have to change all that to get rid of it?  Nope.  I can leave
+## it and just not require it here.  But I will have to enable AHMs
+## that can point to recipes that require 4 args instead of this
+## object...
+
+## OR if I explain it, users have to understand that there is this
+## object that has methods they can call to get inputFile, recipeName
+## and outputFile
+
+## So maybe I will just explain it...  NOT an easy decision for me.
