433d6b27942e6074d4291d95e07e7a0698388701
Marc Carlson <mcarlson@fhcrc.org>
2014-05-15 21:53:08 +0000

    Adds resource and file to start working out how to push AHMs into json that can be pushed to the new back end
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@90384 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/sqliteToJson.R
@@ -0,0 +1,10 @@
+## Code for creating json records from the sqlite DB.
+
+## RIGHT NOW the json looks sort of record centric.  So I need to make
+## code that takes AHMs and makes them into JSON.
+
+
+## So 1st I need an exemplar AHM (will save one for now in inst/extdata
+
+## So for testing: 
+## load(system.file('extData','inpDrosPsuedo.rda', package='AnnotationHubData'))
