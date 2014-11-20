45a8ec80d28c146fc125a92edac8615cf3d54b66
Martin Morgan <mtmorgan@fhcrc.org>
2014-11-20 00:52:51 +0000

    minor makeUCSC* cleanup
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@96937 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeUCSCChain.R
+++ R/makeUCSCChain.R
@@ -1,14 +1,9 @@
 .ucscBase <- "http://hgdownload.cse.ucsc.edu/"
 
 .fileSize <- function(s) {
-    s1 <- character(0)
-    if(s=="")
-        return(NA_character_)
-    
-    let <- gsub("[0-9]", "",s)
-    no <- gsub("[:A-Z:]", "",s)
-    s1 <- ifelse(let=="M", as.numeric(no)*1000000, as.numeric(no)*1000)
-    s1
+    let <- sub("[0-9.]+", "", s)
+    no <- sub("[:A-Z:]+", "", s)
+    ifelse(let=="M", as.numeric(no)*1000000, as.numeric(no)*1000)
 }  
 
 .get1Resource <- function(url, fileName, verbose=FALSE) {
@@ -16,26 +11,25 @@
     tryCatch({
         if (verbose)
             message(basename(dirname(url)))
+
         html <- htmlParse(url)
         fls <- sapply(html["//pre[2]/a/text()"], xmlValue)
         
         ## extract the file name
         url <- sprintf("%s/%s", url, fls)
-        keep <- grepl(paste0(fileName,"$"), fls)
+        keep <- grepl(paste0(fileName, "$"), fls)
+        url <- url[keep]
         
         ## extract the date time stamp of file.  
-        stamps <- sapply(html["//pre[2]/text()"], xmlValue)
+        stamps <- sapply(html["//pre[2]/text()"], xmlValue)[keep]
         regex <- " *([[:alnum:]-]+) *([:[:alnum:]]+)*.*"
         version <- sub(regex, "\\1_\\2", stamps)
         
         ## extract the file size
-        regex2 <- " *([[:alnum:]-]+) *([:[:alnum:]]+)"
-        size <- gsub(" ","",gsub("\\n","",sub(regex2, "", stamps)), fixed=TRUE)
-        size  <- gsub("-","", size)
-        newSize <- sapply(size, .fileSize)
+        size <- sub(".* ([[:alnum:].]+).*", "\\1", stamps)
+        newSize <- .fileSize(size)
         
-        data.frame(url, version, size, newSize, 
-            stringsAsFactors=FALSE)[keep,,drop=FALSE]
+        data.frame(url, version, size, newSize, stringsAsFactors=FALSE)
     }, error=function(err) {
             warning(basename(dirname(url)), ": ", conditionMessage(err))
             data.frame(url=character(), version=character(), 
@@ -71,15 +65,15 @@
     #b) query ="Xenopus tropicalis", scin="Xenopus (Silurana) tropicalis", taxid="8364"
     #c) query ="Spermophilus tridecemlineatus", scin="Ictidomys tridecemlineatus", taxid="43179"
     
-    tax_ind <- match(c("9601","8364","43179"), taxid)
-    scin_ind <- match(c("Pongo abelii","Xenopus (Silurana) tropicalis",
-        "Ictidomys tridecemlineatus"), scin)
-    if(identical(tax_ind, scin_ind))
-        scin[scin_ind] <- c("Pongo pygmaeus abelii","Xenopus tropicalis",
-            "Spermophilus tridecemlineatus")
+    table <- c("Pongo abelii","Xenopus (Silurana) tropicalis",
+               "Ictidomys tridecemlineatus")
+    idx <- match(scin, table)
+    updt <- !is.na(idx)
+    if (any(updt))
+        scin[updt] <- table[idx[updt]]
     
     ## 3. Results
-    as.integer(taxid)[match(organism, scin)]
+    as.integer(taxid[match(organism, scin)])
 }
 
 .getUCSCResources <- function(fileType, dirName, fileName, verbose=FALSE) {
@@ -87,31 +81,31 @@
     .fileBase <- sprintf("%sgoldenPath", .ucscBase)
     genome_tbl <- rtracklayer::ucscGenomes(organism=TRUE)
     genomes <- genome_tbl$db
-    urls <- sprintf(paste0("%s/%s/", dirName), .fileBase, genomes)
+    urls <- sprintf("%s/%s/%s", .fileBase, genomes, dirName)
     rsrc <- do.call(rbind, lapply(urls, .get1Resource,  
-        fileName= fileName, verbose=verbose))
+        fileName=fileName, verbose=verbose))
     rownames(rsrc) <- basename(rsrc$url)
     
-    ## parse the filename for each file type. 
-    if(fileType=="chain") {
-        rsrc$from <- sub("^([a-z][[:alnum:]]+)To.*.over.chain.gz", "\\1",
-            rownames(rsrc))
-        to <- sub(".*To([A-Z][[:alnum:]]+)\\.over.chain.gz", "\\1", 
-            rownames(rsrc))
-        rsrc$to <- sub("^([A-Z])", "\\L\\1", to, perl=TRUE)
-    }
-    
-    if(fileType=="2bit")
-        rsrc$from <- sub(".2bit","",rownames(rsrc))
+    ## parse the filename for each file type.
+    switch(fileType, chain={
+        rsrc$from <- sub("^([[:alnum:]]+)To[A-Z].*", "\\1", rownames(rsrc))
+        rsrc$to <- sub(".*To([A-Z])([[:alnum:]]+).*", "\\L\\1\\E\\2",
+                       rownames(rsrc), perl=TRUE)
+    }, "2bit"={
+        rsrc$from <- sub(".2bit","", rownames(rsrc))
+    }, {
+        stop("unknown fileType ", sQuote(fileType))
+    })
     
     ## add the organism 
     idx <- match(rsrc$from, genome_tbl$db)
-    rsrc$organism <- as.character(genome_tbl[idx, "organism"])
-    rsrc$organism[which(is.na(rsrc$organism))] <- NA_character_
+    rsrc$organism <- rep(NA_character_, length(idx))
+    rsrc$organism[!is.na(idx)] <- genome_tbl[idx[!is.na(idx)], "organism"]
     
     ## add the taxonmy Id. 
-    rsrc$taxid <- .organismToTaxid(rsrc$organism)
-    rsrc$taxid[which(is.na(rsrc$taxid))]<- NA_character_
+    rsrc$taxid <- rep(NA_character_, length(idx))
+    rsrc$taxid[!is.na(idx)] <- .organismToTaxid(rsrc$organism[!is.na(idx)])
+    
     rsrc
 }
 
