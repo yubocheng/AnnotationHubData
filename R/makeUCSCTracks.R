a62c06808a010c526a0598d679f2061d5abe03a2
Sonali Arora <sarora@fhcrc.org>
2015-03-25 22:20:30 +0000

    code update
    recipe update for Tracks recipe
    - add code for justRunUnitTest to quickly test the recipe
    - remove SourceFile,RDataVersion (Redundant field) 
    - add following field : Location_prefix, DisptachClass, SourceType
    
    - file organization:
    a) trackToGRangesRecipe.R has only 1 function - moved to UCSCTrackImportPreparer-class.R
    b) UCSCTrackImport-utils.R  all functions moved to UCSCTrackImportPreparer-class.R
    c) importFastaFiles.R  contains functions which have are no longer needed
    d) UCSCTrackImportPreparer-class.R  is renamed to makeUCSCTracks.R
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@101186 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeUCSCTracks.R
+++ R/makeUCSCTracks.R
@@ -1,3 +1,60 @@
+# -----------------------------------------------------------------------------
+## Import UCSC track to GRanges
+## 
+
+trackToGRangesRecipe <- function(ahm)
+{
+    outputFile <- outputFile(ahm)
+    if (!file.exists(outputFile)) {
+        trackName <- basename(metadata(ahm)$SourceFile)
+        session <- browserSession()
+        genome(session) <- metadata(ahm)$Genome
+        query <- ucscTableQuery(session, trackName)
+        gr <- track(query, asRangedData = FALSE)
+        save(gr, file=outputFile)
+        ## if (!getOption("AnnotationHub_Use_Disk", FALSE)) {
+        ##     upload_to_S3(outputFile, metadata(ahm)$RDataPath)
+        ## }
+    }
+    outputFile(ahm)
+}
+
+## -----------------------------------------------------------------------------
+## Memo-ize some web access, to avoid unnecessary internet traffic
+## 
+
+.ucscSession <- local({
+    session <- NULL
+    function() {
+        if (is.null(session)) {
+            session <<- browserSession()
+        }
+        session
+    }
+})
+
+.GRangesForUSCSGenome <- local({
+    env <- new.env(parent=emptyenv())
+    function(genome) {
+        if (!exists(genome, envir=env))
+            env[[genome]] <- GRangesForUCSCGenome(genome)
+        env[[genome]]
+    }
+})
+
+.ucscTableQuery <- local({
+    env <- new.env(parent=emptyenv())
+    function(genome, trackName) {
+        if (!exists(genome, envir=env)) {
+            range <- .GRangesForUSCSGenome(genome)
+            env[[genome]] <- ucscTableQuery(.ucscSession(), trackName, range)
+        } else {
+            trackName(env[[genome]]) <- trackName
+        }
+        env[[genome]]
+    }
+})
+
 ###########################################################################
 ## Pre-Processing code to allow saving of "bad" tracks that will not load...
 ## Run these helper functions ahead of time and save output objects
