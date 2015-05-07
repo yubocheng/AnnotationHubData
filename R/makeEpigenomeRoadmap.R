19fc5247831b216c501528153a0901b048ad9897
Sonali Arora <sarora@fhcrc.org>
2015-05-07 22:32:41 +0000

    recipe update  - makeEpigenomeRoadMap Files
    - update code to write better description 
    - update tags which are parsed from metadata file found on the project
    - update the name of the file to be the title 
    - update the sourceurl 
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@103548 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeEpigenomeRoadmap.R
+++ R/makeEpigenomeRoadmap.R
@@ -1,69 +1,133 @@
 .EpigenomeRoadMap <- "http://www.broadinstitute.org/~anshul/projects/roadmap/"
 
-.getEpiFileInfo <- function(urls) {
-    df <- .httrFileInfo(urls, verbose=TRUE) 
-    rdatapath <- sub(.EpigenomeRoadMap, "", urls)
-
-    map <- c(
-        ## broad peaks
-        hotspot.fdr0.01.broad.bed.gz="Broad DNasePeaks at FDR 1%",
-        hotspot.broad.bed.gz="Broad DNasePeaks at no FDR thresholding",
-        broadPeak.gz="Broad ChIP peaks passing the p-value 0.1 threshold",
-        gappedPeak.gz=.expandLine("Broad ChIP peaks (passing p-value 0.1) that
-            contain at least one narrow peak passing a pvalue of 0.01."),
-        ## narrow peaks
-        hotspot.fdr0.01.peaks.bed.gz=.expandLine("Narrow DNasePeaks in FDR 1%
-            hotspots called using HotSpot"),
-        hotspot.all.peaks.bed.gz=.expandLine("Genome-wide tag density peak calls,
-            (not restricted to hotspots, thresholded or unthresholded) called
-            using HotSpot peak caller"),
-        .macs2.narrowPeak.gz=.expandLine("Narrow DNasePeaks called using MACS2
-            with a p-value threshold of 0.01"),
-        narrowPeak.gz=.expandLine("Narrow ChIP peaks were called using MACSv2
-            with a p-value threshold of 0.01."))
-    description <- character(length(urls))
-    for (i in seq_along(map))
-        description[grep(names(map)[i], urls)] <- map[[i]]
-    
-    cbind(df, rdatapath=rdatapath, description=description, 
-        stringsAsFactors =FALSE)
-}
-
-.getPeakCalls <- function(url, tag, justRunUnitTest) {
-    ans <- tryCatch ({
-        message(basename(url))
-        result <- GET(url)
-        stop_for_status(result)
-        html <- content(result)
-        
-        fls <- sapply(html["//li/a/@href"], as.character)
-        fls <- fls[!grepl(".*(/|README.txt).*" , fls)]
-        urls <- paste0(url, fls)
-        
-        if(justRunUnitTest)
-            urls <- urls[1:5]
-        
-        ## get file information for each file. 
-        .getEpiFileInfo(urls) ## step takes time!!
-        
+.readEpiFilesFromWebUrl <- function(url, fileType, justRunUnitTest) {
+    tryCatch ({
+       table <- XML::readHTMLTable(url)[[1]]
+       fnames <- grep(fileType, as.character(table$Name), value=TRUE) 
+       fileurl <- paste0(url, fnames)
+       
+       if(justRunUnitTest)
+           fileurl <- fileurl[1:10]
+       fileurl
+       
     }, error=function(err) {
-        warning(basename(url), ": ", conditionMessage(err), immediate.=TRUE)
-        data.frame(url=character(), version=character(),
-                   stringsAsFactors=FALSE)
-    })
-    cbind(ans, tag=tag)
+        warning(url, ": ", conditionMessage(err), immediate.=TRUE)
+        data.frame(url=character(), stringsAsFactors=FALSE)
+    }) 
+    
 }
 
-.getEpigenomeRoadMapPeaks <- function(justRunUnitTest) {
+.EpiFilesMetadata <- function() {
+    metafname <-"metadata/EID_metadata.tab"
+    read.delim(paste0(.EpigenomeRoadMap, metafname))
+}
+
+.EpiFilesTags <- function(url) {
+    ## get columns F,O,D from metadata
+    metadata <- .EpiFilesMetadata()
+    filename <- basename(url)
+    celltype <- gsub("-.*","", filename)
+    idx <- match(celltype, metadata[,"EID"])
+    metadata <- metadata[,c("EID", "GROUP","MNEMONIC", "STD_NAME")]
+    fod <- vapply(idx, function(x) 
+        paste0(as.character(unlist(metadata[x,])), collapse=", ")
+        , character(1))
+    fod <- gsub("[\x01-\x1f\x7f-\xff]", "", fod) # remove \xa0
+    ## parse the url to get which kind of  files
+    tempurl <- gsub(.EpigenomeRoadMap, "", url)
+    out <- strsplit(tempurl, "/")
+    mat <- do.call(rbind, out)
+    matTags <- apply(mat, 1, function(x) paste0(x[1:3], collapse=", "))
+    
+    paste("EpigenomeRoadMap", matTags, fod, sep=", ")
+}
+
+
+.MiscEpiFiles <- function(fileurls) {
+    ## get the file size and  the file info 
+    df <- .httrFileInfo(fileurls, verbose=TRUE) 
+    
+    ## these files are downloaded from the web 
+    rdatapath <- sub(.EpigenomeRoadMap, "", fileurls)
+  
+    ## add tags
+    tags <- .EpiFilesTags(fileurls)
+    
+    ## description 
+    description <- .EpiFilesDescription(fileurls)
+    
+    cbind(df, rdatapath, tags, description, stringsAsFactors=FALSE)
+}
+
+.EpiFilesDescription <- function(fileurls) {  
+    ## description 
+    ## refer : http://egg2.wustl.edu/roadmap/web_portal/processed_data.html    
+    map <- c(
+        ## broad peaks- DNase-seq
+        hotspot.fdr0.01.broad.bed.gz=.expandLine("Broad domains on enrichment for 
+             DNase-seq for consolidated epigenomes from EpigenomeRoadMap 
+             Project"),
+        hotspot.broad.bed.gz=.expandLine("Broad domains on enrichment for 
+            DNase-seq for consolidated epigenomes from EpigenomeRoadMap 
+            Project"),
+        
+        broadPeak.gz=.expandLine("Narrow ChIP-seq peaks for consolidated 
+            epigenomes from EpigenomeRoadMap Project"),
+        gappedPeak.gz=.expandLine("Gapped ChIP-seq peaks for consolidated 
+            epigenomes from EpigenomeRoadMap Project"),
+        narrowPeak.gz=.expandLine("Narrow ChIP-seq peaks for consolidated 
+            epigenomes from EpigenomeRoadMap Project"),
+    
+        ## narrow peaks
+        hotspot.fdr0.01.peaks.bed.gz=.expandLine("Narrow DNasePeaks for 
+            consolidated epigenomes from EpigenomeRoadMap Project"),
+        hotspot.all.peaks.bed.gz=.expandLine("Narrow DNasePeaks for 
+            consolidated epigenomes from EpigenomeRoadMap Project"),
+        .macs2.narrowPeak.gz=.expandLine("Narrow DNasePeaks for 
+            consolidated epigenomes from EpigenomeRoadMap Project"), 
+        
+        ## Big Wig Files
+        
+        fc.signal.bigwig=.expandLine("Bigwig File containing  fold enrichment 
+            signal tracks for consolidated epigenomes from EpigenomeRoadMap
+            Project"),
+        pval.signal.bigwig=.expandLine("Bigwig File containing -log10(p-value) 
+            signal tracks for consolidated epigenomes from EpigenomeRoadMap
+            Project"), 
+        imputed.pval.signal.bigwig=.expandLine("Bigwig File containing 
+            -log10(p-value) signal tracks for unconsolidated epigenomes from 
+            EpigenomeRoadMap Project"))
+        
+    description <- character(length(fileurls))
+    for (i in seq_along(map))
+        description[grep(names(map)[i], fileurls)] <- map[[i]]
+    
+    description
+}
+
+## PEAK CONSOLIDATED FILES
+## these files are to be re-inserted 
+.peakConsolidatedEpiFiles <- function(justRunUnitTest) {
     paths <- c(broadPeaks="peaks/consolidated/broadPeak/",
-               narrowPeaks="peaks/consolidated/narrowPeak/")
-    urls <- setNames(paste0(.EpigenomeRoadMap, paths), names(paths))
-
-    do.call(rbind, Map(.getPeakCalls, urls, names(urls), justRunUnitTest))
+               narrowPeaks="peaks/consolidated/narrowPeak/", 
+               gappedPeaks="peaks/consolidated/gappedPeak/" )
+    fileurls <- lapply(paste0(.EpigenomeRoadMap, paths), function(x) {
+        .readEpiFilesFromWebUrl(x, "gz", justRunUnitTest)
+    })
+    .MiscEpiFiles(unlist(fileurls))
 }
+
+
+
+
+
+
+
+
+
 
 makeEpigenomeRoadmap <- function(currentMetadata, justRunUnitTest=FALSE) {
-    rsrc <- .getEpigenomeRoadMapPeaks(justRunUnitTest)
+    rsrc <- .peakConsolidatedEpiFiles(justRunUnitTest=TRUE)
     
     description <- rsrc$description
     title <- basename(rsrc$fileurl)
