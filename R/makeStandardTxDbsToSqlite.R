ce604d4b442b48e37f92a4d6161ea0f27d35c159
Valerie Obenchain <vobencha@fhcrc.org>
2016-12-22 16:59:44 +0000

    add makeStandardTxDbsToSqlite() recipe
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHubData@125397 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/makeStandardTxDbsToSqlite.R
@@ -0,0 +1,108 @@
+### =======================================================================
+### makeStandardTxDbsToSqlite
+### -------------------------------------------------------------------------
+###
+
+## This recipe extracts the sqlite files from the 'standard'
+## TxDb packages in the current Bioconductor repo:
+## 
+##   http://www.bioconductor.org/packages/release/BiocViews.html#___TxDb.
+
+## Recipe should be run after new TxDbs have been generated (right before
+## the next release).
+
+## Returns list of loaded TxDb objects
+.getTxDbs <- function(install=FALSE) {
+    dbNames <- recommendPackages("TxDb", use.release=FALSE)
+    ## FDb.UCSC.tRNAs is a FeatureDb package; remove it
+    dbNames <- dbNames[-match("FDb.UCSC.tRNAs", dbNames)]
+    if (install) {  ## download, install
+        require(BiocInstaller)
+        lapply(dbNames, function(xx) {
+            if (!require(xx, character.only=TRUE)) {
+                biocLite(xx, ask=FALSE)
+            }
+        })
+    }
+    lapply(dbNames, require, character.only=TRUE)
+    res <- lapply(dbNames, get)
+    names(res) <- dbNames
+    res
+}
+
+.TxDbPkgMetadataFromObjs <- function(txdbs, biocversion) {
+     title <- paste0(names(txdbs), '.sqlite')
+     species <- unlist(lapply(txdbs,
+         function(x){m <- metadata(x); m[m$name=='Organism', 2] }))
+     taxonomyId <- as.integer(unlist(lapply(txdbs, 
+         function(x) {
+             m <- metadata(x) 
+             id <- m[m$name=='TaxID', 2]
+             if (!length(id))
+                 id <- m[m$name=='Taxonomy ID', 2]
+             id
+         })))
+
+     sourceVersion <- sapply(txdbs, 
+         function(x) {
+             m <- metadata(x) 
+             paste0('UCSC transcript based annotations generated ', 
+                    strptime(m[m$name=='Creation time', 2], "%Y-%m-%d")) 
+         }, simplify=FALSE)
+     url <- list(c("http://genome.ucsc.edu/", 
+                   "http://hgdownload.cse.ucsc.edu/goldenPath"))
+     list(title=title,
+          species=species,
+          taxonomyId=taxonomyId,
+          genome=rep("UCSC genomes", length(title)),
+          sourceUrl=rep(url, length(title)),
+          sourceVersion=sourceVersion,
+          description=paste("UCSC transcript based annotations for", species),
+          rDataPath=paste0("ucsc/standard/", biocversion, "/",title))
+}
+
+makeStandardTxDbsToAHM <- function(currentMetadata, justRunUnitTest = FALSE, 
+                                    BiocVersion = biocVersion()) {
+    if (length(BiocVersion) > 1L)
+        stop("length(BiocVersion) must == 1L")
+
+    txdbs <- .getTxDbs()
+    meta <- .TxDbPkgMetadataFromObjs(txdbs, biocversion=BiocVersion)
+    Map(AnnotationHubMetadata,
+        AnnotationHubRoot=currentMetadata$AnnotationHubRoot,
+        Description=meta$description,
+        Genome=meta$genome,
+        SourceUrl=meta$sourceUrl,
+        SourceVersion=meta$sourceVersion,
+        Species=meta$species,
+        TaxonomyId=meta$taxonomyId,
+        Title=meta$title,
+        RDataPath=meta$rDataPath,
+        MoreArgs=list(
+            BiocVersion=BiocVersion,
+            Coordinate_1_based=TRUE, ## TRUE unless it "needs" to be FALSE
+            DataProvider="UCSC",
+            Maintainer="Bioconductor Maintainer <maintainer@bioconductor.org>",
+            RDataClass="TxDb",
+            DispatchClass="SQLiteFile",
+            SourceType="FASTA",
+            RDataDateAdded = Sys.time(),
+            Recipe="AnnotationHubData:::extractTxDbSqlite",
+            Tags=c("UCSC", "Transcript", "Annotation"))) 
+}
+
+## Load the object and call saveDb()
+extractTxDbSqlite <- function(ahm) {
+    dbFile <- metadata(ahm)$Title
+    dbName <- sub('.sqlite','',dbFile)
+    txdbs <- .getTxDbs()
+    txdb <- txdbs[[dbName]]
+    outputPath <- file.path(metadata(ahm)$AnnotationHubRoot,
+                            basename(metadata(ahm)$RDataPath))
+    if (!isSingleString(outputPath)) 
+        stop("'outputPath' must be a single string")
+    sqliteCopyDatabase(dbconn(txdb), outputPath)
+    outputFile(ahm)
+}
+
+makeAnnotationHubResource("TxDbFromPkgsImportPreparer", makeStandardTxDbsToAHM)
