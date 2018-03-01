257e33026459a7bc4afcc300965ff4ac4888c629
lshep <shepherl@gmail.com>
2018-03-01 10:37:11 -0500

    Updated template for new single resource from flutre

--- inst/scripts/singleContributedResourceTemplate.R
+++ inst/scripts/singleContributedResourceTemplate.R
@@ -1,8 +1,33 @@
 ## Community contributed resources.
 
 ## -----------------------------------------------------------------------
-## Timothee Flutre's GRanges from GFF 
+## Timothee Flutre's GRanges from GFF
 ## -----------------------------------------------------------------------
+
+## Vitis vinifera URGI IGGP12Xv2_V3-20
+metadata <- AnnotationHubMetadata(
+    Description="Gene Annotation for Vitis vinifera",
+    Genome="IGGP12Xv2",
+    Species="Vitis vinifera",
+    SourceUrl="http://doi.org/10.15454/1.5009072354498936E12",
+    SourceLastModifiedDate=as.POSIXct("2018-02-16"),
+    SourceVersion="3.0",
+    RDataPath="community/tflutre/Vvinifera_URGI_IGGP12Xv2_V3-20.gff3.Rdata",
+    TaxonomyId=29760,
+    Title="Vvinifera_URGI_IGGP12Xv2_V3-20.gff3.Rdata",
+    BiocVersion=package_version("3.6"),
+    Coordinate_1_based=TRUE,
+    DataProvider="URGI",
+    Maintainer="Timothée Flutre <timothee.flutre@inra.fr>",
+    RDataClass="GRanges",
+    DispatchClass="GRanges",
+    SourceType="GFF",
+    RDataDateAdded=as.POSIXct(Sys.time()),
+    Recipe=NA_character_,
+    PreparerClass="None",
+    Tags=c("GFF", "URGI", "Gene", "Transcript", "Annotation"),
+    Notes="compare to the original GFF3 file, chromosomes were slightly renamed to be compatible with the reference genome"
+)
 
 ## Vitis vinifera URGI IGGP12Xv2 V3
 metadata <- AnnotationHubMetadata(
@@ -13,7 +38,7 @@ metadata <- AnnotationHubMetadata(
     SourceLastModifiedDate=as.POSIXct("2017-11-17"),
     SourceVersion="3.0",
     RDataPath="community/tflutre/Vvinifera_URGI_IGGP12Xv2_V3.gff3.Rdata",
-    TaxonomyId=29760, 
+    TaxonomyId=29760,
     Title="Vvinifera_URGI_IGGP12Xv2_V3.gff3.Rdata",
     BiocVersion=package_version("3.6"),
     Coordinate_1_based=TRUE,
@@ -38,7 +63,7 @@ metadata <- AnnotationHubMetadata(
     SourceLastModifiedDate=as.POSIXct("2014-04-17"),
     SourceVersion="2.1",
     RDataPath="community/tflutre/",
-    TaxonomyId=29760L, 
+    TaxonomyId=29760L,
     Title="Vvinifera_CRIBI_IGGP12Xv0_V2.1.gff3.Rdata",
     BiocVersion=package_version("3.3"),
     Coordinate_1_based=TRUE,
@@ -63,7 +88,7 @@ metadata <- AnnotationHubMetadata(
     SourceLastModifiedDate=as.POSIXct("2010-03-19"),
     SourceVersion="1.0",
     RDataPath="community/tflutre/",
-    TaxonomyId=29760L, 
+    TaxonomyId=29760L,
     Title="Vvinifera_Genoscope_IGGP12Xv0_V1.0.gff3.Rdata",
     BiocVersion=package_version("3.3"),
     Coordinate_1_based=TRUE,
@@ -88,7 +113,7 @@ metadata <- AnnotationHubMetadata(
     SourceLastModifiedDate=as.POSIXct("2007-10-09"),
     SourceVersion="1.0",
     RDataPath="community/tflutre/",
-    TaxonomyId=29760L, 
+    TaxonomyId=29760L,
     Title="Vvinifera_Genoscope_IGGP8X_V1.0.gff3.Rdata",
     BiocVersion=package_version("3.3"),
     Coordinate_1_based=TRUE,
@@ -106,7 +131,7 @@ metadata <- AnnotationHubMetadata(
 
 ## -----------------------------------------------------------------------
 ## upload to S3
-file <- 
+file <-
 bucket <- getOption("ANNOTATION_HUB_BUCKET_NAME", "annotationhub")
 remotePath <- paste0(metadata(metadata)$RDataPath, metadata(metadata)$Title)
 res <- upload_to_S3(file, remotePath, bucket)
