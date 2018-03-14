f64f5d022bac7661dfb2987628e724db6e33823d
lshep <shepherl@gmail.com>
2018-03-14 09:14:08 -0400

    Fix XML::htmlParse induced ERRROR
       - urls updated from http to https; htmlParse cannot handle secure
       - read in file first with getURL, then pass to htmlParser

--- R/makeGencodeGFF.R
+++ R/makeGencodeGFF.R
@@ -1,8 +1,8 @@
-# recipe to get GFF3 files from Genecode. 
-# importtant links 
+# recipe to get GFF3 files from Genecode.
+# importtant links
 #http://www.gencodegenes.org/releases/
 #ftp site: ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human
-# readme file for genecode project 
+# readme file for genecode project
 #ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/_README.TXT
 
 # for gff3 files
@@ -16,7 +16,7 @@
 
 # only gff3 files will be added - since both gtf and gff3 contain same
 # data, but gff3 is better (Herve) .These files will not be stored as
-# a GRanges on amazon s3. 
+# a GRanges on amazon s3.
 
 .gencodeBaseUrl <- "ftp://ftp.sanger.ac.uk/pub/gencode/"
 
@@ -33,27 +33,27 @@
     map <- c(
       annotation.gff3.gz=.expandLine("Gene annotations
           on reference chromosomes from Gencode"),
-      chr_patch_hapl_scaff.annotation.=.expandLine("Gene annotation 
+      chr_patch_hapl_scaff.annotation.=.expandLine("Gene annotation
           on reference-chromosomes/patches/scaffolds/haplotypes from Gencode"),
-      polyAs=.expandLine("files contain polyA signals, polyA sites and 
+      polyAs=.expandLine("files contain polyA signals, polyA sites and
           pseudo polyAs manually annotated by HAVANA from only the refrence
           chromosome"),
-      wayconspseudos=.expandLine("pseudogenes predicted by the Yale 
+      wayconspseudos=.expandLine("pseudogenes predicted by the Yale
           & UCSC pipelines, but not by Havana on reference chromosomes"),
       long_noncoding_RNAs=.expandLine("sub-set of the main annotation files
-          on the reference chromosomes. They contain only the lncRNA genes. 
-          Long non-coding RNA genes are considered the genes with any of 
-          those biotypes: 'processed_transcript', 'lincRNA', 
-          '3prime_overlapping_ncrna', 'antisense', 'non_coding', 
+          on the reference chromosomes. They contain only the lncRNA genes.
+          Long non-coding RNA genes are considered the genes with any of
+          those biotypes: 'processed_transcript', 'lincRNA',
+          '3prime_overlapping_ncrna', 'antisense', 'non_coding',
           'sense_intronic' , 'sense_overlapping' , 'TEC' , 'known_ncrna'."),
-      tRNAs =.expandLine("tRNA structures predicted by tRNA-Scan on 
-          reference chromosomes"),  
-      transcripts.fa.gz=.expandLine("Protein-coding transcript sequences 
+      tRNAs =.expandLine("tRNA structures predicted by tRNA-Scan on
+          reference chromosomes"),
+      transcripts.fa.gz=.expandLine("Protein-coding transcript sequences
           on reference chromosomes Fasta file"),
-      translations.fa.gz=.expandLine("Translations of protein-coding 
+      translations.fa.gz=.expandLine("Translations of protein-coding
           transcripts on reference chromosomes Fasta file"),
-      lncRNA_transcripts.fa.gz=.expandLine("Long non-coding RNA 
-          transcript sequences on reference chromosomes Fasta file.") 
+      lncRNA_transcripts.fa.gz=.expandLine("Long non-coding RNA
+          transcript sequences on reference chromosomes Fasta file.")
       )
     description <- character(length(fileurls))
     for (i in seq_along(map))
@@ -66,19 +66,20 @@
     # this information is curated from Gencode's website
     # link - http://www.gencodegenes.org/releases/
     if (species=="Human")
-      tblurl <- "http://www.gencodegenes.org/releases/"
-    else 
-      tblurl <- "http://www.gencodegenes.org/mouse_releases/"
-    
+      tblurl <- "https://www.gencodegenes.org/releases/"
+    else
+      tblurl <- "https://www.gencodegenes.org/mouse_releases/"
+
     ## read in the table
-    tbl <- XML::readHTMLTable(tblurl, header=TRUE, stringsAsFactors=FALSE)    
+    http <- RCurl::getURL(tblurl)
+    tbl <- XML::readHTMLTable(http, header=TRUE, stringsAsFactors=FALSE)
     tbl <- tbl[[1]]
     tblheader <- gsub("\n", "", colnames(tbl))
     tblheader = trimws(tblheader)
     colnames(tbl) = tblheader
-    
+
     idx <- which(tbl[,"GENCODE release"]==release)
-    tbl[idx,"Genome assembly version"]  
+    tbl[idx,"Genome assembly version"]
 }
 
 
@@ -88,9 +89,9 @@
     speciesUrl <- ifelse(species=="Human", "Gencode_human/", "Gencode_mouse/")
     dirurl = paste0(.gencodeBaseUrl, speciesUrl, "release_", release, "/")
     names(dirurl) <- paste0(species,"_", release)
-    
-    fileurls <-.gencodeFileFromUrl(dirurl) 
-    
+
+    fileurls <-.gencodeFileFromUrl(dirurl)
+
     if (tolower(filetype)=="gff")
        idx <-  grep("gff3", fileurls)
     if(tolower(filetype)=="fasta")
@@ -98,30 +99,30 @@
     fileurls <- fileurls[idx]
 
     if(length(idx)==0)
-     stop("No files found.") 
+     stop("No files found.")
 
      if(justRunUnitTest)
         fileurls <- fileurls[1:2]
-   
-    ## tags 
+
+    ## tags
     filename <- basename(fileurls)
     filename <- sub(".gz","", filename)
-    tags <- gsub("[.]",",",filename)   
-  
-    ## description 
+    tags <- gsub("[.]",",",filename)
+
+    ## description
     description <- .gencodeDescription(fileurls)
-     
+
     ## rdatapath - these files will be made into GRanges and stored on S3.
-    #rdatapath <- paste0("gencode/", species, "/release_", release,"/", 
+    #rdatapath <- paste0("gencode/", species, "/release_", release,"/",
     #    basename(fileurls), ".Rda")
-    
+
     rdatapath <- sub(.gencodeBaseUrl, "", fileurls)
-    
+
 
     ## get date and size for files
     df <- .httrFileInfo(fileurls)
     rownames(df) <- NULL
-    
+
     ## species, taxid, genome
     scSpecies <- ifelse(species=="Human", "Homo sapiens", "Mus musculus")
     taxid <- ifelse(species=="Human", 9606L, 1090L)
@@ -130,8 +131,8 @@
     scSpecies <- rep(scSpecies, length(fileurls))
     taxid <- rep(taxid, length(fileurls))
     genome <- .gencodeGenome(species, release)
-    genome <- rep(genome, length(fileurls))  
-  
+    genome <- rep(genome, length(fileurls))
+
     cbind(df, rdatapath, description, tags, species=scSpecies, taxid, genome,
          stringsAsFactors=FALSE)
 }
@@ -140,11 +141,11 @@
 ## STEP 1: make function to process metadata into AHMs
 makeGencodeGFFsToAHMs <- function(currentMetadata, justRunUnitTest, BiocVersion){
 
-    ## important - here you need to know which species and release you want to 
-    ## add files for.  
-    rsrc <- .gencodeSourceUrls(species="Human", release="23", filetype="gff", 
+    ## important - here you need to know which species and release you want to
+    ## add files for.
+    rsrc <- .gencodeSourceUrls(species="Human", release="23", filetype="gff",
          justRunUnitTest)
-       
+
     description <- rsrc$description
     title <- basename(rsrc$fileurl)
     genome <- rsrc$genome
@@ -156,9 +157,9 @@ makeGencodeGFFsToAHMs <- function(currentMetadata, justRunUnitTest, BiocVersion)
     species <- rsrc$species
     rdatapath <- rsrc$rdatapath
     taxid <- rsrc$taxid
-    
+
     Map(AnnotationHubMetadata,
-        Description=description, 
+        Description=description,
         Genome=genome,
         SourceUrl=sourceUrls,
         SourceSize=SourceSize,
@@ -166,9 +167,9 @@ makeGencodeGFFsToAHMs <- function(currentMetadata, justRunUnitTest, BiocVersion)
         SourceVersion=sourceVersion,
         Species=species,
         RDataPath=rdatapath,
-        TaxonomyId=taxid, 
+        TaxonomyId=taxid,
         Title=title,
-	Tags=tags, 
+        Tags=tags,
         MoreArgs=list(
           BiocVersion=BiocVersion,
           Coordinate_1_based = TRUE,
