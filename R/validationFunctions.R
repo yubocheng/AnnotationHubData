2fbd55da50cc421a05c34d01b2ce4c045f7cb334
lshep <lori.shepherd@roswellpark.org>
2020-02-25 13:56:01 -0500

    Add XML as valid source type

--- R/validationFunctions.R
+++ R/validationFunctions.R
@@ -60,7 +60,7 @@ getValidSourceTypes <- function(){
                              "mzXML", "NCBI/blast2GO", "NCBI/ensembl",
                              "NCBI/UniProt", "RDA", "RData", "Simulated", "tab",
                              "tar.gz", "TSV", "TwoBit", "TXT", "UCSC track",
-                             "VCF", "XLS/XLSX", "Zip")
+                             "VCF", "XLS/XLSX", "XML", "Zip")
 
     expectedSourceTypes
 
